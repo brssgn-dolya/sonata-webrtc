7217759bc03443e4672141470056d17a52741f8b
Boris <brssgn@icloud.com>
2025-01-07 00:05:44 +0200

    Add SPM support

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.6
+import PackageDescription
+
+let package = Package(
+    name: "SonataWebRTC",
+    platforms: [.iOS(.v15)],
+    products: [
+        .library(
+            name: "WebRTC",
+            targets: ["WebRTC"]),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "WebRTC",
+            url: "https://github.com/jitsi/webrtc/releases/download/v124.0.1/WebRTC.xcframework.zip",
+            checksum: "f4f3788d23415be250651da5ed69ccce462995a7a6cd0f742fa55168c55800d8"
+        ),
+    ]
+)
